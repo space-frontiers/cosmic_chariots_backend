d240c8a8551f80f9e187d67b0dca19f2fd7c7925
Albert Bautista <bautista.albert@gmail.com>
2022-11-20 14:56:05 -0600

    Update auth.js to include first_name

--- utils/auth.js
+++ utils/auth.js
@@ -26,8 +26,8 @@ module.exports = {
 
     return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function ({ first_name, email, _id }) {
+    const payload = { first_name, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
